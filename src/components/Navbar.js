b07533fb09e02ae5b40fc3da375004114a348e6b
Dalgy Garcia <dalgygarcia120@gmail.com>
2022-02-01 00:26:31 -0300

    adding navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+function Navbar() {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <span className='display-4'>SHOES UP</span>
+            <ul className='menu navbar-nav me-auto mb-2 mb-lg-0'>
+                <li className='list nav-item'>
+                    <a className='nav-link active' href="#">HOMBRE</a></li>
+                <li className='list nav-item'>
+                    <a className='nav-link active' href="#">MUJER</a></li>
+                <li className='list nav-item'>
+                    <a className='nav-link active' href="#">NIÑO</a></li>
+                <li className='list nav-item'>
+                    <a className='nav-link active' href="#">DEPORTE</a></li>
+                <li className='list nav-item'>
+                    <a className='nav-link active' href="#">COLECCIONES</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
